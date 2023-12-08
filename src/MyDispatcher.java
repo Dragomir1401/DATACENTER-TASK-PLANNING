14b9b8258501b84bbc98f6dd1a4d88c61980b8a8
Dragomir Andrei <andreidragomir1401@stud.acs.upb.ro>
2023-12-08 20:51:18 +0200

    all tests work locally

--- src/MyDispatcher.java
+++ src/MyDispatcher.java
@@ -1,18 +1,36 @@
 /* Implement this class. */
 
 import java.util.List;
+import java.util.Objects;
 
 public class MyDispatcher extends Dispatcher {
+    // The minimum queue size of a host storage variable
     int minQueueSize = Integer.MAX_VALUE;
-    long minWorkLeft = Integer.MAX_VALUE;
-    int lastHostId = 0;
 
+    // The minimum work left of a host storage variable
+    long minWorkLeft = Integer.MAX_VALUE;
+
+    // The last host id of a host storage variable
+    int lastHostId = -1;
+
+
+    /**
+     * Creates a dispatcher.
+     *
+     * @param algorithm the scheduling algorithm to be used by the dispatcher
+     * @param hosts     the list of hosts available for task execution
+     */
     public MyDispatcher(SchedulingAlgorithm algorithm, List<Host> hosts) {
         super(algorithm, hosts);
     }
 
+
+    /**
+     * Add a task to a host queue using different scheduling algorithms.
+     * @param task the task to be added
+     */
     @Override
-    public void addTask(Task task) {
+    public synchronized void addTask(Task task) {
         // Switch between the scheduling algorithms
         switch (algorithm) {
             case ROUND_ROBIN -> addTaskRR(task);
@@ -23,59 +41,75 @@ public class MyDispatcher extends Dispatcher {
         }
     }
 
+
+    /**
+     * Add a task to a host queue using the Size Interval Task Assignment algorithm.
+     * @param task the task to be added
+     */
     public void addTaskSITA(Task task) {
         // Add the task to the host with the same type
         int taskType = task.getType().ordinal();
         hosts.get(taskType).addTask(task);
     }
 
+
+    /**
+     * Add a task to a host queue using the Least Work Left algorithm.
+     * @param task the task to be added
+     */
     public void addTaskLWL(Task task) {
-        // Firstly find the host with the least work left
+        Host selectedHost = null;
+        long minWorkLeft = Long.MAX_VALUE;
+
+        // Find the host with the least work left, and in case of a tie, select the one with the smaller ID
+        int lastCheckedHostId = 0;
+        int hostCounter = 0;
         for (Host h : hosts) {
-            // Print the queue size of each host
-            if (h.getWorkLeft() < minWorkLeft) {
-                minWorkLeft = h.getWorkLeft();
+            long workLeft = h.getWorkLeft();
+            if (workLeft < minWorkLeft || (workLeft == minWorkLeft && hostCounter < lastCheckedHostId)) {
+                lastCheckedHostId = hostCounter;
+                minWorkLeft = workLeft;
+                selectedHost = h;
             }
+            hostCounter++;
         }
 
-        for (Host host : hosts) {
-            // Assign task to the host with the smallest queue size
-            if (host.getWorkLeft() == minWorkLeft) {
-                // Add the task to the host
-                host.addTask(task);
-
-                // Reset the minWorkLeft
-                minWorkLeft = Integer.MAX_VALUE;
-
-                return;
-            }
+        // Assign task to the selected host
+        if (selectedHost != null) {
+            selectedHost.addTask(task);
         }
     }
 
+
+    /**
+     * Add a task to a host queue using the Round Robin algorithm.
+     * @param task the task to be added
+     */
     public void addTaskRR(Task task) {
-        // Add the task to the host
-        hosts.get(lastHostId).addTask(task);
-        lastHostId = (lastHostId + 1) % hosts.size();
+        // Compute the next host id and add the task to the host with that id
+        int nextHostId = (lastHostId + 1) % hosts.size();
+        hosts.get(nextHostId).addTask(task);
+        lastHostId = nextHostId;
     }
 
+
+    /**
+     * Add a task to a host queue using the Shortest Queue algorithm.
+     * @param task the task to be added
+     */
     public void addTaskSQ(Task task) {
         // Firstly find the host with the smallest queue size
         for (Host h : hosts) {
-            // Print the queue size of each host
             if (h.getQueueSize() < minQueueSize) {
                 minQueueSize = h.getQueueSize();
             }
         }
 
+        // Assign task to the host with the smallest queue size
         for (Host host : hosts) {
-            // Assign task to the host with the smallest queue size
             if (host.getQueueSize() == minQueueSize) {
-                // Add the task to the host
                 host.addTask(task);
-
-                // Reset the minQueueSize
                 minQueueSize = Integer.MAX_VALUE;
-
                 return;
             }
         }
--- src/MyHost.java
+++ src/MyHost.java
@@ -1,77 +1,125 @@
 /* Implement this class. */
+import java.util.Objects;
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class MyHost extends Host {
-    // Create blocking queue
-    private final PriorityBlockingQueue<Task> queue = new PriorityBlockingQueue<>(11, (t1, t2) -> Integer.compare(t2.getPriority(), t1.getPriority()));
-    volatile double finishTime = 0;
+    // Blocking queue for the tasks of the host
+    private final PriorityBlockingQueue<Task> queue = new PriorityBlockingQueue<>(10, (t1, t2) -> {
+        if (t1.getPriority() == t2.getPriority()) {
+            if (t1.getStart() == t2.getStart()) {
+                if (Objects.equals(t1.getDuration(), t2.getDuration())) {
+                    return t1.getId() - t2.getId();
+                } else {
+                    return (int) (t1.getDuration() - t2.getDuration());
+                }
+            } else {
+                return t1.getStart() - t2.getStart();
+            }
+        } else {
+            return t2.getPriority() - t1.getPriority();
+        }
+    });
+
+    // The time when the working task started executing
+    volatile double currentTime = 0;
+
+    // The working task
     volatile Task workingTask = null;
+
+    // The stop flag
     volatile Boolean stop = false;
 
+
+    /**
+     * Runs the first task from the queue of the host.
+     */
     @Override
     public void run() {
-        // Make the host busy for the duration of the task
+        // While the shutdown method was not called
         while (!stop) {
+            // If the queue still has tasks
             if (!queue.isEmpty()) {
                 try {
+                    // Store the current time before starting the task
+                    currentTime = Timer.getTimeDouble();
+
+                    // Take the first task from the queue and simulate its execution
                     workingTask = queue.take();
-                    finishTime = Timer.getTimeDouble();
-
-                    // Print the task
                     sleep(workingTask.getLeft());
-                    workingTask.setLeft(0);
 
-                    // Finish the task
+                    // Set left time to 0 and finish the task
+                    workingTask.setLeft(0);
                     workingTask.finish();
+
+                    // Set the working task to null to be it is cleared
                     workingTask = null;
                 } catch (InterruptedException e) {
-                    workingTask.setLeft(workingTask.getDuration() - (Math.round(Timer.getTimeDouble() - finishTime) * 1000));
+                    // If another task interrupts the working task
+                    // Set the left time of the working task to the remaining time
+                    workingTask.setLeft(workingTask.getDuration() - (Math.round(Timer.getTimeDouble() - currentTime) * 1000));
+
+                    // Add the working task back to the queue to be executed later
                     queue.offer(workingTask);
                 }
             }
         }
     }
 
+
+    /**
+     * Adds a task to the queue of the host.
+     * @param task the task to be added
+     */
     @Override
     public synchronized void addTask(Task task) {
-        // Add the task to the queue
+        // Add the task to the queue anyway
         queue.offer(task);
 
-        // Print queue until now
-//        System.out.println("Host: " + this);
-//        System.out.println("Queue: " + queue);
-
-        // Check if the working task is preemptive
+        // If the working task exists and is preemptive
         if (workingTask != null && workingTask.isPreemptible()) {
-            // Check if the entering task has higher priority
+            // If the entering task has a higher priority than the working task
             if (task.getPriority() > workingTask.getPriority()) {
-                // Interrupt the working task
+                // Interrupt the working task to execute the entering task
                 this.interrupt();
             }
         }
     }
 
+
+    /**
+     * Returns the size of the queue of the host.
+     * @return the size of the queue of the host
+     */
     @Override
     public synchronized int getQueueSize() {
         synchronized (queue) {
             if (workingTask != null) {
+                // If the working task exists, return the size of the queue + 1
                 return queue.size() + 1;
             } else {
+                // Else return the size of the queue
                 return queue.size();
             }
         }
     }
 
+
+    /**
+     * Returns the total work left of the host, meaning the sum
+     * of the work left of the working task and the work left
+     * of all the tasks in the queue.
+     * @return the total work left of the host
+     */
     @Override
     public synchronized long getWorkLeft() {
         long workLeft = 0;
         synchronized (queue) {
-            // Compute the work left of the working task
+            // Compute the work left of the working task and add it to the total work left
             if (workingTask != null) {
-                workLeft += workingTask.getDuration() - (Math.round(Timer.getTimeDouble() - finishTime) * 1000);
+                workLeft += workingTask.getDuration() - (Math.round(Timer.getTimeDouble() - currentTime) * 1000);
             }
 
-            // Add all the tasks' work left from the queue except the working task
+            // Add all the tasks that are in the queue to the total work left
             for (Task task : queue) {
                 workLeft += task.getLeft();
             }
@@ -80,9 +128,13 @@ public class MyHost extends Host {
         return workLeft;
     }
 
+
+    /**
+     * Stops the host.
+     */
     @Override
     public void shutdown() {
-        // Stop the host
+        // Set the stop flag to true
         stop = true;
     }
 }
